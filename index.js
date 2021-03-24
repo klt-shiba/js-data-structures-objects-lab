9497cc5698af2b5f93a343786e5f38a08144205b
Colin Tseng <klt.aus@yahoo.com>
2021-03-24 22:04:05 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+let driver = {
+    key: "value"
+}
+
+function updateDriverWithKeyAndValue(a, b, c) {
+    let d = {...a};
+    d[b] = c; 
+    return a, d
+}
+function destructivelyUpdateDriverWithKeyAndValue(a, b, c) {
+    a[b] = c; 
+    return a;
+}
+
+function deleteFromDriverByKey(a, b) {
+    let d = {...a};
+    delete d[b]; 
+    return a, d;
+}
+
+function destructivelyDeleteFromDriverByKey(a, b) {
+    delete a[b]; 
+    return a;
+}
+
+
+destructivelyDeleteFromDriverByKey(driver, key)
+
+deleteFromDriverByKey(driver, b)
+
+destructivelyUpdateDriverWithKeyAndValue(driver, key, value);
+
+console.log(updateDriverWithKeyAndValue(driver, address, "11 Broadway"))
